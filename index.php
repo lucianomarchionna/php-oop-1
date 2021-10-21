9487a3bd6223712c3abce98e4f32711dd2b25348
Luciano <lucco6823@gmail.com>
2021-10-21 12:00:09 +0200

    Aggiunta Costruttore

--- index.php
+++ index.php
@@ -5,6 +5,15 @@
    => all'interno della classe è definito almeno un metodo
     - vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà */
 
-    
+    class ‘Movie’ {
+        public $titol;
+        public $original_language;
+        public $vote ;
+        public $duration;
+
+        function __construct($_vote){
+            $this->vote = $_vote;
+        }
+    }
 
 ?>
